6d86b848e4b00157f953368e92b4db580378b81f
Rafa <bsapphireprueba@gmail.com>
2020-07-07 09:23:04 -0500

    Configuracion de Pipes y Conexion a DB pg y Typeorm

---
+++ src/tasks/task.repository.ts
@@ -0,0 +1,8 @@
+import { Repository, EntityRepository } from "typeorm";
+import { Task } from "./task.entity";
+
+
+@EntityRepository(Task)
+export class Taskrepository extends Repository<Task>{
+    
+}
